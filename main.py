1a332205e6c534f26e2c548728b2dd18667ebec9
cH6noota <morichika.816@gmail.com>
2019-10-27 04:38:07 +0900

    Improve

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def handle_post(event):
         r = requests.post(url,headers =head ,json={'to':line_user_id ,'messages':[x1,x2,x3]})
         
     elif new["postback"]["data"]=="action=second":
-        print()
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=new))
         #DB 書き込み
 
 
