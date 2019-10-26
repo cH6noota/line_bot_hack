e4249662d53f50877b48cc043fbfd844b0ca5a5b
chikaramorita <chikaramorita@chikaramoritanoMacBook-Air.local>
2019-10-26 19:34:00 +0900

    Improve

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent,TextMessage,TextSendMessage
 import os
 import json
-#from funcs import id_check_func 
+from funcs import id_check_func 
 
 
 app=Flask(__name__)
@@ -31,8 +31,8 @@ def callback():
 def handle_message(event):
     texx=str(event)
     new = json.loads(texx)
-    #text_1=id_check_func(new["source"]["userId"])
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=new["source"]["userId"]))
+    text_1=id_check_func(new["source"]["userId"])
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text_1))
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=new["source"]["userId"]) )
     get_mess=new["message"]["text"]
 
