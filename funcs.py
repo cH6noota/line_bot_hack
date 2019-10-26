a8ebf615bf40dcbf8fa62baf8514bf48baf8d068
cH6noota <morichika.816@gmail.com>
2019-10-26 23:42:12 +0900

    Improve

--- funcs.py
+++ funcs.py
@@ -18,7 +18,7 @@ def id_check_func(line_user_id):
     url ="http://ik1-334-27288.vs.sakura.ne.jp/hack10/test.php?line_user_id="+line_user_id+"&appUserId="+appUserId
     r = requests.get(url)
     return appUserId
-def talk_func(appUserId , message):
+def talk_func(line_user_id, appUserId , message):
     url="https://api.repl-ai.jp/v1/dialogue"
     apikey="z1frsJuv5oyEjoFYiHTPQP79WZvthvysGEAQ24bW"
 
@@ -27,11 +27,22 @@ def talk_func(appUserId , message):
 
     r = requests.post(url,headers=header, json=body)
     data = json.loads(r.text)["systemText"]['expression']
-    data=data.split("%%")
-    send=""
-    for i in data:
-        if send=="":
-            send=send+i
-        else:
-            send=send+"\n"+i
-    return send
+    if len(data.split("%%"))>0:
+        data=data.split("%%")
+        send=""
+        for i in data:
+            if send=="":
+                send=send+i
+            else:
+                send=send+"\n"+i
+        return send
+    if data=="場所_ok":
+        #一時保管用 CSV
+        url ="http://ik1-334-27288.vs.sakura.ne.jp/hack10/form/form_data1.php?line_user_id="+line_user_id+"&plase="+message
+        r = requests.get(url)
+        return "場所の入力が完了しました"
+    elif data=="時間_ok":
+        url ="http://ik1-334-27288.vs.sakura.ne.jp/hack10/form/form_data1.php?line_user_id="+line_user_id+"&date="+message
+        r = requests.get(url)
+        return "時間"
+
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from funcs import id_check_func ,talk_func
 
 app_id=id_check_func("U1277ea3bd6292d55ddb9a1a7d01ae1fe")
 
-print(talk_func(app_id,"っd"))
+print(talk_func("U1277ea3bd6292d55ddb9a1a7d01ae1fe",app_id,"予約"))
 
