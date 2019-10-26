cee4b00a5beb3bc6631dcdaf43dd4a40da8f2315
cH6noota <morichika.816@gmail.com>
2019-10-27 00:05:46 +0900

    Improve

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent,TextMessage,TextSendMessage,PostbackEvent
 import os
 import json
-from funcs import id_check_func ,talk_func 
+from funcs import id_check_func ,talk_func ,show_database
 
 app=Flask(__name__)
 #環境変数の取得
@@ -42,13 +42,14 @@ def handle_message(event):
 @handler.add(PostbackEvent)
 def handle_post(event):
     texx=str(event)
-    new = json.loads(texx)
-    get_date= new["postback"]["params"]["datetime"].split("T")
-    date=get_date[0]
-    time_data=get_date[1]
-    line_user_id=new["source"]["userId"]
+    #new = json.loads(texx)
+    #get_date= new["postback"]["params"]["datetime"].split("T")
+    #date=get_date[0]  time_data=get_date[1]
 
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=date+time_data))
+    #line_user_id=new["source"]["userId"]
+    #show_database(get_date)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=texx))
+
 
     
 if __name__=="__main__":
