99eafa4278c11f23e6a470e8dd18870cd599d980
cH6noota <morichika.816@gmail.com>
2019-10-27 14:24:18 +0900

    Improve

--- funcs.py
+++ funcs.py
@@ -178,6 +178,7 @@ def mental_db(message ,line_user_id ,place):
         conn.close()
 
 def checker(num ,date ,place):
+    num=int(num)
     if num==0 or num==1 or num==2 or num==3 or num==4 or num==5 or num==6 or num==7 or num==8:
         return False
     conn = pymysql.connect(
